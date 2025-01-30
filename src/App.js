c0753295dcfb78c908c41ffd4d5ac7805527db06
Aracely0504 <aracelybc90@gmail.com>
2025-01-29 22:53:54 -0800

    added bootstrap to project

--- src/App.js
+++ src/App.js
@@ -5,21 +5,23 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/Aracely0504" target="_blank">
-          {" "}
-          Aracely Campos{" "}
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Aracely0504/react-weatherforecast-app"
-          target="_blank"
-        >
-          open-sourced of Github{" "}
-        </a>{" "}
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/Aracely0504" target="_blank">
+            {" "}
+            Aracely Campos{" "}
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Aracely0504/react-weatherforecast-app"
+            target="_blank"
+          >
+            open-sourced of Github{" "}
+          </a>{" "}
+        </footer>
+      </div>
     </div>
   );
 }
