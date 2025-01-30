3944b7cec4cb5361d07ebec70279f6f86711fb8c
Aracely0504 <aracelybc90@gmail.com>
2025-01-29 22:22:38 -0800

    Added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,23 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/Aracely0504" target="_blank">
+          {" "}
+          Aracely Campos{" "}
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Aracely0504/react-weatherforecast-app"
+          target="_blank"
+        >
+          open-sourced of Github{" "}
+        </a>{" "}
+      </footer>
+    </div>
+  );
 }
