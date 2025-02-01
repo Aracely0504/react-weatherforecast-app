523cc694d416686af553ff6ecea36fba7954f87f
Aracely0504 <aracelybc90@gmail.com>
2025-01-31 21:04:30 -0800

    updated content

--- src/App.js
+++ src/App.js
@@ -9,15 +9,8 @@ export default function App() {
         <h1> Weather App</h1>
         <footer>
           This project was coded by{" "}
-          <a href="https://github.com/Aracely0504" target="_blank">
-            {" "}
-            Aracely Campos{" "}
-          </a>{" "}
-          and is{" "}
-          <a
-            href="https://github.com/Aracely0504/react-weatherforecast-app"
-            target="_blank"
-          >
+          <a href="https://github.com/Aracely0504"> Aracely Campos </a> and is{" "}
+          <a href="https://github.com/Aracely0504/react-weatherforecast-app">
             open-sourced of Github{" "}
           </a>{" "}
         </footer>
